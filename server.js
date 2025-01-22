d2f644353fe03f64642102dce049997e904fc7f1
Marcelo Rigoto <marcelorigoto1@gmail.com>
2025-01-22 10:49:48 +0100

    Build 22_01

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const serveIndex = require('serve-index');
+const path = require('path');
+
+const app = express();
+
+// Serve static files and enable directory listing
+app.use('/data', express.static(path.join(__dirname, 'data')));
+app.use('/data', serveIndex(path.join(__dirname, 'data'), { icons: true }));
+
+// Serve the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = 8000;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
